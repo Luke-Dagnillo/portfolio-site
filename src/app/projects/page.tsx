ea376708d2563727f9fc467048ede42cb25fadd4
Luke Dagnillo <lucianodagnillo@gmail.com>
2025-06-01 22:51:49 -0700

    fix: added valid export to projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Luciano Dagnillo</h1>
+      <p className="mt-4 text-lg">MS ECE Student | Embedded Systems & Machine Learning</p>
+      <p className="mt-2">Welcome to my portfolio website!</p>
+    </main>
+  );
+}
